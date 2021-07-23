c4c73bca3b4d9af978a45d6fed9bb5c8283c9c5b
Benedikt Walch <benx.bw@gmail.com>
2021-07-23 11:36:14 +0200

    Landing page with link to pokemon page, basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Pokemon from "./pages/Pokemon";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <footer className="footer">
+          <nav>
+            <Link to="/">
+              <FontAwesomeIcon icon={faHome} />
+            </Link>
+          </nav>
+        </footer>
+        <Switch>
+          <Route path="/pokemon">
+            <Pokemon />
+          </Route>
+          <Route path="/">
+            <Home Link={Link} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
