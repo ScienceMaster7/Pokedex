78069e7e1d7776f1c5de7248493e5e6ecf050d6e
Benedikt Walch <benx.bw@gmail.com>
2021-07-24 09:40:41 +0200

    Functionality for Load next and load previous pokemon button and for go back button

--- src/pages/Singlepokemon.js
+++ src/pages/Singlepokemon.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { useState } from "react/cjs/react.development";
 import "./Singlepokemon.css";
 
@@ -19,18 +19,50 @@ export default function Singlepokemon() {
   }, [id]);
 
   function Renderpokemon() {
+    let history = useHistory();
+    function goback() {
+      return history.push("/pokemon");
+    }
+    function loadprevious() {
+      return history.push(`/pokemon/${id - 1}`);
+    }
+    function loadnext() {
+      return history.push(`/pokemon/${Number(id) + 1}`);
+    }
     if (isloading || pokemon === null) {
       return "Loading...";
     }
 
     return (
-      <section>
-        <h1>{pokemon.name}</h1>
+      <section className="pokemon__container">
+        <h1 className="pokemon__header">{pokemon.name}</h1>
         <img
           className="pokemon__img--big"
           src={pokemon.sprites.other["official-artwork"].front_default}
           alt=""
         />
+
+        <div className="pokemon__button__container">
+          {id > 1 && (
+            <button
+              className="pokemon__button__singlebutton"
+              onClick={loadprevious}
+            >
+              Load previous
+            </button>
+          )}
+          <button className="pokemon__button__singlebutton" onClick={goback}>
+            Go back
+          </button>
+          {id < 898 && (
+            <button
+              className="pokemon__button__singlebutton"
+              onClick={loadnext}
+            >
+              Load next
+            </button>
+          )}
+        </div>
       </section>
     );
   }
